606455ba2b538b083e2c6f20ff25016f67425f4f
aymak91 <amakyu@gmail.com>
2023-11-06 17:45:12 -0800

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from "next/head"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>1337Code</title>
+        <meta name="description" content="Web application that contains leetcode problems and video solutions" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+
 }
