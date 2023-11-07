74aaf8da5728c977a52529bb54df66161de8eaae
aymak91 <amakyu@gmail.com>
2023-11-06 18:47:54 -0800

    add login modal

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar/Navbar"
+import AuthModal from "@/components/Modals/AuthModal"
 import React from 'react';
 
 type AuthPageProps = {
@@ -11,6 +12,10 @@ const AuthPage:React.FC<AuthPageProps> = () => {
         <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
             <div className="max-w-7xl mx-auto">
                 <Navbar />
+                <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
+                    <img src="/hero.png/" alt="Hero img" />
+                </div>
+                <AuthModal/>
             </div>
         </div>
     )
