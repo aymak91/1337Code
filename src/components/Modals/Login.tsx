26f7fb1cc2e4ee0683db286ffd3478f354bf706f
aymak91 <amakyu@gmail.com>
2023-11-08 10:49:28 -0800

    add login functionality

--- src/components/Modals/Login.tsx
+++ src/components/Modals/Login.tsx
@@ -1,6 +1,10 @@
 import { authModalState } from '@/atoms/authModalAtom';
 import React from 'react';
 import { useSetRecoilState } from 'recoil';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import {auth} from '@/firebase/firebase'
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 type LoginProps = {
     
@@ -11,20 +15,52 @@ const Login:React.FC<LoginProps> = () => {
     const handleClick = (type: "login" | "register" | "forgotPassword") => {
         setAuthModalState((prev) => ({...prev, type}))
     }
+    const [inputs, setInputs] = useState({email: "", password: ""});
+
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,] = useSignInWithEmailAndPassword(auth);
+    
+    
+    const router = useRouter();
+    
+    const onHandleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputs((prev) => ({...prev, [e.target.name]: e.target.value}))
+    }
+
+    const handleLogin = async (e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!inputs.email || !inputs.password) return alert("Please fill in all fields");
+
+        try {
+            console.log('ifdfsdfnsdlnglk')
+            const newUser = await signInWithEmailAndPassword(inputs.email, inputs.password)
+            if (!newUser) return;
+            router.push("/")
+        } catch (error: any) {
+            alert(error.message)
+        }
+    }    
+
+    console.log(user, "user")
+
     return (
-        <form action="" className='space-y-6 px-6 py-4'>
+        <form action="" className='space-y-6 px-6 py-4' onSubmit={handleLogin}>
             <h3 className='text-xl font-medium text-white'>Sign into 1337Clone</h3>
             <div>
                 <label htmlFor='email' className='text-sm font-medium block mb-2 text-gray-300'>
                     Email
                 </label>
-                <input type="email" name='email' id='email' placeholder='name@domain.com' className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white' />
+                <input onChange={onHandleInputChange} type="email" name='email' id='email' placeholder='name@domain.com' className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white' />
             </div>
             <div>
                 <label htmlFor='password' className='text-sm font-medium block mb-2 text-gray-300'>
                     Password
                 </label>
-                <input type="password" name='password' id='password' placeholder='Password' className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white' />
+                <input onChange={onHandleInputChange} type="password" name='password' id='password' placeholder='Password' className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white' />
             </div>
             <button type="submit" className='w-full text-white focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s'>
                 Login
